bf39583cfb0164f316f164e16e1a93228ec92960
zack <zphricz@gmail.com>
2015-11-07 14:09:34 -0800

    -Code formatting

--- main.cpp
+++ main.cpp
@@ -8,60 +8,60 @@
 using namespace std;
 
 static void error(char *name) {
-  printf("Usage: %s [Board_x Board_y] [Screen_x Screen_y] [nthreads = %d]\n",
-         name, thread::hardware_concurrency());
-  exit(1);
+    printf("Usage: %s [Board_x Board_y] [Screen_x Screen_y] [nthreads = %d]\n",
+           name, thread::hardware_concurrency());
+    exit(1);
 }
 
 int main(int argc, char *argv[]) {
-  SDL_DisplayMode display;
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    exit(1);
-  }
-  SDL_GetCurrentDisplayMode(0, &display);
-  atexit(SDL_Quit);
-  srand(time(NULL));
-
-  int screen_width = display.w;
-  int screen_height = display.h;
-  int num_cells_x = screen_width / 2;
-  int num_cells_y = screen_height / 2;
-  int num_threads = thread::hardware_concurrency();
-
-  switch (argc) {
-  case 6:
-    num_threads = atoi(argv[5]);
-    if (num_threads <= 0) {
-      error(argv[0]);
+    SDL_DisplayMode display;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        exit(1);
     }
-  case 5:
-    screen_width = atoi(argv[3]);
-    screen_height = atoi(argv[4]);
-    if (screen_width <= 0 || screen_height <= 0) {
-      error(argv[0]);
-    }
-  case 3:
-    num_cells_x = atoi(argv[1]);
-    num_cells_y = atoi(argv[2]);
-    if (num_cells_x <= 0 || num_cells_y <= 0) {
-      error(argv[0]);
-    }
-  case 1:
-    break;
-  default:
-    error(argv[0]);
-  }
+    SDL_GetCurrentDisplayMode(0, &display);
+    atexit(SDL_Quit);
+    srand(time(NULL));
 
-  bool full_screen;
-  if (screen_width == display.w && screen_height == display.h) {
-    full_screen = true;
-  } else {
-    full_screen = false;
-  }
+    int screen_width = display.w;
+    int screen_height = display.h;
+    int num_cells_x = screen_width / 2;
+    int num_cells_y = screen_height / 2;
+    int num_threads = thread::hardware_concurrency();
 
-  Threadpool::set_num_threads(num_threads);
-  PerfSoftScreen scr(screen_width, screen_height, "Life", full_screen, true);
-  Game g(num_cells_x, num_cells_y, &scr);
-  g.run();
-  return 0;
+    switch (argc) {
+    case 6:
+        num_threads = atoi(argv[5]);
+        if (num_threads <= 0) {
+            error(argv[0]);
+        }
+    case 5:
+        screen_width = atoi(argv[3]);
+        screen_height = atoi(argv[4]);
+        if (screen_width <= 0 || screen_height <= 0) {
+            error(argv[0]);
+        }
+    case 3:
+        num_cells_x = atoi(argv[1]);
+        num_cells_y = atoi(argv[2]);
+        if (num_cells_x <= 0 || num_cells_y <= 0) {
+            error(argv[0]);
+        }
+    case 1:
+        break;
+    default:
+        error(argv[0]);
+    }
+
+    bool full_screen;
+    if (screen_width == display.w && screen_height == display.h) {
+        full_screen = true;
+    } else {
+        full_screen = false;
+    }
+
+    Threadpool::set_num_threads(num_threads);
+    PerfSoftScreen scr(screen_width, screen_height, "Life", full_screen, true);
+    Game g(num_cells_x, num_cells_y, &scr);
+    g.run();
+    return 0;
 }
